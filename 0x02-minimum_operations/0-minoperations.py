2f25cd222c2e5edd5b4234f68577af255c345b5c
Owinnie <99328720+Owinnie@users.noreply.github.com>
2023-01-11 03:10:49 +0300

    draft file

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" Count no. of copy paste operations """
+
+
+def minOperations(n):
+    """Calculates the fewest number of operations
+    needed to result in exactly n H characters in
+    the file.
+    
+    Returns:
+        integer of 0 if impossible
+    """
+    copied = 0
+    pasted = 1  # In a text file, there is a single character H
+    operations = 0
+    
+    while pasted < n:
+      ...
+    
+    return 0
