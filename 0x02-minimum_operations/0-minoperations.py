661decaa7327ef2d1b418709571fb00517629bbe
Owinnie <tillawinnie@gmail.com>
2023-01-11 13:15:04 +0300

    pycodestyle errors

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -6,43 +6,43 @@ def minOperations(n):
     """Calculates the fewest number of operations
     needed to result in exactly n H characters in
     the file.
-    
+
     Returns:
         integer of 0 if impossible
     """
     copied = 0
     pasted = 1  # In a text file, there is a single character H
     operations = 0
-    
+
     while pasted < n:
-    
+
         # copy operation
         if copied == 0:
             copied = pasted
             operations += 1
-      
+
         # paste operation
         if pasted == 1:
             pasted += copied
             operations += 1
             continue
-          
+
         #
         rem = n - pasted
-      
+
         if rem < clipboard:
             return 0
-          
-        if rem % pasted !=0
+
+        if rem % pasted != 0:
             pasted += copied
             operations += 1
-          
+
         else:
             copied = pasted
             pasted += copied
             operations += 2
-          
+
         if pasted == n:
             return operations
-    
+
         return 0
