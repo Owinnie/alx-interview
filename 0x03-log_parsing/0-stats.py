8e59f6e617ecb4e185c5e3e0d45b71af7f8f489f
Owinnie <99328720+Owinnie@users.noreply.github.com>
2023-01-17 12:38:37 +0300

    test 1

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -4,35 +4,34 @@
 
 import sys
 
-s_codes = dict()
+cache = {'200': 0, '301': 0, '400': 0, '401': 0,
+         '403': 0, '404': 0, '405': 0, '500': 0}
 total_size = 0
-count = 0
+counter = 0
 
 try:
     for line in sys.stdin:
-        ls_lines = line.split(" ")  # split line at spaces
-        if len(ls_lines) > 4:
-            file_size = int(ls_lines[-1])
-            code = ls_lines[-2]
-            if code in s_codes.keys():
-                s_codes[code] += 1
-            else:
-                s_codes[code] = 0
-            total_size += file_size
-            count += 1
+        line_list = line.split(" ")
+        if len(line_list) > 4:
+            code = line_list[-2]
+            size = int(line_list[-1])
+            if code in cache.keys():
+                cache[code] += 1
+            total_size += size
+            counter += 1
 
-        if count == 10:
-            count -= count  # reset count
-            print("File size: {}".format(total_size))
-            for k, v in sorted(s_codes.items()):
-                if v != 0:
-                    print("{}: {}".format(k, v))
+        if counter == 10:
+            counter = 0
+            print('File size: {}'.format(total_size))
+            for key, value in sorted(cache.items()):
+                if value != 0:
+                    print('{}: {}'.format(key, value))
 
-except Exception as e:
+except Exception as err:
     pass
 
 finally:
-    print(f"File size: {total_size}")
-    for k, v in sorted(s_codes.items()):
-        if v != 0:
-            print("{}: {}".format(k, v))
+    print('File size: {}'.format(total_size))
+    for key, value in sorted(cache.items()):
+        if value != 0:
+            print('{}: {}'.format(key, value))
