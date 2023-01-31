5048020e86792c21e9022ab7a9682c4003fe6e64
Owinnie <tillawinnie@gmail.com>
2023-01-31 16:58:55 +0300

    first attempt

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -22,4 +22,33 @@ if n < 4:
     print("N must be at least 4")
     exit(1)
 
+col = set()
+pos = set()  # row + column
+neg = set()  # rom - column
 
+result = []
+board = [["0"] * n for i in range(n)]
+
+def btrack(r):
+    if r == n:
+        cp = ["".join(row) for row in board]
+        result.append(cp)
+        return
+
+    for c in range(n):
+        if c in col or (r + c) in pos or (r - c) in neg:
+            continue
+
+        col.add(c)
+        pos.add(r + c)
+        neg.add(r - c)
+        board[r][c] = "Q"
+
+        btrack(r + 1)
+
+        col.remove(c)
+        pos.remove(r + c)
+        neg.remove(r - c)
+        board[r][c] = "0"
+btrack(0)
+print(result)
