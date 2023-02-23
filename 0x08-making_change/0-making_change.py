e861faa482af49337d8b45e5e8453ea63d212cd2
Owinnie <tillawinnie@gmail.com>
2023-02-24 02:17:42 +0300

    6th attmpt

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -29,7 +29,7 @@ def makeChange(coins, total):
             if c == total:
                 count += 1
                 return count
-        
+
         dp = [total + 1] * (total + 1)
         dp[0] = 0
         for a in range(1, total + 1):
