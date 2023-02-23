da4a9ce857713139a0ae502f28f06bbfd9245500
Owinnie <tillawinnie@gmail.com>
2023-02-24 02:05:30 +0300

    3rd attmpt

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -22,8 +22,19 @@ def makeChange(coins, total):
     dp = [total + 1] * (total + 1)
     dp[0] = 0
 
-    for a in range(1, total + 1):
+    if total <= 0:
+        return 0
+    elif sum(coins) != total:
+        return -1
+    else:
+        count = 0
         for c in coins:
-            if (a - c) >= 0:
-                dp[a] = min(dp[a], 1 + dp[a - c])
-    return dp[total] if dp[total] != total + 1 else -1
+            if c == total:
+                count += 1
+                return count
+
+        for a in range(1, total + 1):
+            for c in coins:
+                if (a - c) >= 0:
+                    dp[a] = min(dp[a], 1 + dp[a - c])
+        return dp[total] if dp[total] != total + 1 else -1
