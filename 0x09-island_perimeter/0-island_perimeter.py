7709d39a534e9a467d66ee8a7879f5a16597f465
Owinnie <tillawinnie@gmail.com>
2023-03-03 02:22:16 +0300

    island perimeter

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -18,4 +18,26 @@ that returns the perimeter of the island described in grid:
 
 def island_perimeter(grid):
     """Island Perimeter"""
-    pass
+    visited = set()
+
+    # Depth first search function
+    def dfs(i, j):
+        # Base case
+        if i >= len(grid) or j >= len(grid[0]) or i < 0 or j < 0 or\
+                grid[i][j] == 0:
+                    return 1
+        if (i, j) in visited:
+            return 0
+
+        visited.add((i, j))
+
+        p = dfs(i, j + 1)
+        p += dfs(i + 1, j)
+        p += dfs(i, j - 1)
+        p += dfs(i - 1, j)
+        return p
+
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            if grid[i][j]:
+                return dfs(i, j)
